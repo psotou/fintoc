363bdd1c38525e822551e3851dc01a0df5eb8766
Pascual Soto Uribe <pascualsu@gmail.com>
2021-08-29 13:31:49 -0400

    old stuff removed

--- fintoc/link.go
+++ fintoc/link.go
@@ -6,28 +6,6 @@ import (
 	"log"
 )
 
-func (a *APIClient) LinkAll() []Link {
-	var links []Link
-	dataBytes, _ := a.GetReq(LinksAll)
-	err := json.Unmarshal(dataBytes, &links)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	return links
-}
-
-func (a *APIClient) LinkOne(linkdId string) *NewLink {
-	var link Link
-	url := fmt.Sprintf(LinkURL, linkdId)
-	dataBytes, _ := a.GetReq(url)
-	err := json.Unmarshal(dataBytes, &link)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	newL := &NewLink{client: a, Link: link, linkToken: linkdId}
-	return newL
-}
-
 type NewLink struct {
 	client *APIClient
 	// We make this field anonymous in order to directly get
--- fintoc/movement.go
+++ fintoc/movement.go
@@ -6,36 +6,13 @@ import (
 	"log"
 )
 
-func (n *NewAccount) MovementAll() []Movement {
-	var movements []Movement
-
-	url := fmt.Sprintf(Accounts+MovementsAll+LinkToken, n.Id, n.linkToken)
-	dataBytes, _ := n.client.GetReq(url)
-	err := json.Unmarshal(dataBytes, &movements)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	return movements
-}
-
-func (n *NewAccount) MovementOne(movementId string) Movement {
-	var movement Movement
-	url := fmt.Sprintf(Accounts+Movements+LinkToken, n.Id, movementId, n.linkToken)
-	dataBytes, _ := n.client.GetReq(url)
-	err := json.Unmarshal(dataBytes, &movement)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	return movement
+type MovementClient struct {
+	*NewAccount
 }
 
 type MovementM interface {
 	All() []Movement
 	Get(string) Movement
-}
-
-type MovementClient struct {
-	*NewAccount
 }
 
 func (n *NewAccount) All() []Movement {
